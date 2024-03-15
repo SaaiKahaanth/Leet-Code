6977cc5d01ff606c8954e5134687f83242210f53
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-03-15 22:46:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def partitionString(self, s):
+       d={""}
+       c=1
+       for i in s:
+           if i  not in d:
+                d.add(i)
+            else:
+               c+=1
+               d.clear()
+               d.add(i)
+       return c
+                  
+
+
+       
