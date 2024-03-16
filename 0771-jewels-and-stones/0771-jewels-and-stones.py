8c04122fdcf28be403b60d840bbc45cbffd62427
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-03-16 23:50:07 +0530

    Time: 6 ms (97.98%), Space: 11.7 MB (63.05%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        c=0
+        for i in stones:
+            if i in jewels:
+                c+=1
+        return c
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        
