1e3d13b776862143501332c7b628d2ce6691d00d
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-03-30 18:52:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        return ransomNote in magazine
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        
