a695e7ac78399fda1cf821a28a42c68034f939bb
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-04-14 16:10:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def mySqrt(self, x):
+        res=0
+        if x==0:
+            return 0
+        if x==1:
+            return 1
+
+
+        l,r=0,x
+        while l<=r:
+            m=l+((r-l)//2).
+
+
+            if m**2<x:
+                l=m+1
+                res=m
+                
+
+                
+            elif m**2>x:
+                r=m-1
+                 
+            else:
+                return m
+        
+        return res
+        """
+        :type x: int
+        :rtype: int
+        """
+        
