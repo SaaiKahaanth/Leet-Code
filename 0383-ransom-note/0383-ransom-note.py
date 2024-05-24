2bf5d2229572c63d217200958ac03ba6f40b2141
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-05-24 06:11:56 +0530

    Time: 43 ms (63.01%), Space: 11.6 MB (94.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,32 @@
 class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        return ransomNote in magazine
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
+    def canConstruct(self, a, b):
+        dict={}
+        for i in b:
+            if i not in dict:
+                dict[i]=1
+            else:
+                dict[i]+=1
+        for i in a:
+            if i in dict and dict[i]>0:
+                dict[i]-=1
+            else :
+                return False
+        return True
+
+
+
+
+        if a in b:
+
+            j=b.index(a[0])
+
+            for i in range(len(a)):
+
+                if a[i]!=b[j]:
+                    return False
+                b.remove(b[j])
+            else :
+                j+=1
+
+        
         
